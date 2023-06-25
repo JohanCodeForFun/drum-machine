146e0522c68fa440e9814d034f4e9681c636b071
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-06-25 21:20:17 +0200

    working drum machine with keypress

--- src/components/DrumMachine.tsx
+++ src/components/DrumMachine.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { drums } from "../assets/drumsData.js";
 
 type drums = {
@@ -13,15 +14,37 @@ export const DrumMachine = () => {
   //   }
   // }
 
+  useEffect(() => {
+    document.addEventListener("keydown", detectKeyPressed, true);
+  }, []);
+
+  const detectKeyPressed = (e: KeyboardEvent<HTMLDivElement>) => {
+    drums.find((element: drums) => {
+      if (element.key !== e.key.toUpperCase()) {
+        return;
+      } else if (element.key === e.key.toUpperCase()) {
+        const button = document.getElementById(element.key);
+        button?.click();
+        button?.focus();
+      }
+    });
+  };
+
   return (
     <>
       <div className="pad-bank">
         {drums.map((drum: drums) => {
           return (
-            <div className="drum-pad" id={drum.song} key={drum.key}
-              onClick={() => document.getElementById(drum.key)?.play()}>
-              <audio className="clip" id={drum.key} src={drum.url}></audio>{drum.key}
-          </div>
+            <div
+              className="drum-pad"
+              id={drum.song}
+              key={drum.key}
+              onClick={() => document.getElementById(drum.key)?.play()}
+              onKeyDown={detectKeyPressed}
+            >
+              <audio className="clip" id={drum.key} src={drum.url}></audio>
+              {drum.key}
+            </div>
           );
         })}
       </div>
