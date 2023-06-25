1c91fbffb68db76249ea9057eedb87a5f1f6fb7d
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-06-25 19:12:00 +0200

    add id property to drums

--- src/assets/drumsData.js
+++ src/assets/drumsData.js
@@ -2,22 +2,52 @@ import drum1 from "./BD_LXR_Valve11_A1.wav";
 import drum2 from "./HH_LXR_Valve_11.wav";
 import hiHat from "./HH_LXR_Valve_11.wav";
 import foot from "./Tom_LXR_Valve16.wav"
+import clap from "./Clap_LXR_Valve_1.wav"
 
 export const drums = [
   {
+    id: "Q",
     name: "drum1",
     sound: drum1
   },
   {
+    id: "W",
     name: "drum2",
     sound: drum2
   },
   {
+    id: "E",
     name: "hi-hat",
     sound: hiHat
   },
   {
+    id: "A",
     name: "foot",
     sound: foot
+  },
+  {
+    id: "S",
+    name: "clap",
+    sound: clap
+  },
+  {
+    id: "D",
+    name: "clap",
+    sound: clap
+  },
+  {
+    id: "Z",
+    name: "clap",
+    sound: clap
+  },
+  {
+    id: "X",
+    name: "clap",
+    sound: clap
+  },
+  {
+    id: "C",
+    name: "clap",
+    sound: clap
   },
 ];
