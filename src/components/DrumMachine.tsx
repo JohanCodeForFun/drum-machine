ce25397f0c3f29fc9d2c50457893bec723d9f7f1
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-06-25 19:12:14 +0200

    map drums programmaticaly

--- src/components/DrumMachine.tsx
+++ src/components/DrumMachine.tsx
@@ -1,25 +1,27 @@
-import {drums} from "../assets/drumsData.js"
+import { drums } from "../assets/drumsData.js";
 
 type drums = {
-  name: string,
-  sound: string
-}
+  name: string;
+  sound: string;
+};
 
 export const DrumMachine = () => {
-
   function handleClick(input: string) {
-    new Audio(input).play()
+    new Audio(input).play();
   }
 
-// const drumButtons = drums.map(drum => <button onClick={handleClick(drum.sound)} key={drum.name}>{drum.name}</button>)
- 
   return (
     <>
-      <h1>Druuummms!</h1>
-      <div>
-        {/* {drumButtons} */}
-        {/* {drums.map(btn => <button>{btn.name}</button>)} */}
+      <div className="pad-bank">
+        {drums.map((drum) => {
+          return (
+            <div className="drum-pad" id={drum.name} key={drum.id}>
+              <audio className="clip" id={drum.id} src={drum.sound}></audio>{drum.id}
+              <button key={drum.name}>{drum.name}</button>
+          </div>
+          );
+        })}
       </div>
     </>
-  )
-}
+  );
+};
